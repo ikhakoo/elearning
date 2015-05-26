7ee7bac973b092f8aa1f589cbb2f0fe925eca38e
ikhakoo <ikhakoo@gmail.com>
2015-05-26 00:18:26 -0400

    updating dashboard controller and adding todo list

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < ApplicationController
   def index
   end
+
+  def tracks
+  	# @user = current_user
+  	# @enrollment = @user.enrollments.find(:user_id)
+  	# @courses = Course.all
+  	# @lessons = @user.lessons.where(course: @courses.@enrollments.course_id)
+  end
 end
