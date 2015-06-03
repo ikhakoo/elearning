ff129a5aee0e64cc70a8ce731f9b696ab7b9902a
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-06-03 13:32:42 -0400

    Changed up the schema to remove the step stuff

--- db/schema.rb
+++ db/schema.rb
@@ -69,13 +69,6 @@ ActiveRecord::Schema.define(version: 20150603033756) do
     t.integer  "lesson_count"
   end
 
-  create_table "steps", force: :cascade do |t|
-    t.integer  "chapter_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,6 @@ def destroy_that_shit
   print "Shit "
   Lesson.delete_all
   print "Up "
-  Step.delete_all
-  print "Son"
   Chapter.delete_all
   print "!"
 end
