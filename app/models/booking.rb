813ac6de9694fb3f4997bde3b94044b98da15868
ikhakoo <ikhakoo@gmail.com>
2015-06-06 03:34:22 -0400

    adding helpers for schedule and booking times

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,21 @@
 class Booking < ActiveRecord::Base
 	belongs_to :user
 	has_many :schedules
+
+	def hour_options
+		(opening...closing).map do |hour|
+			if hour >= 12
+				nicehour = hour - 12
+				ampm = "pm"
+			else
+				nicehour = hour
+				ampm = "am"
+			end
+			if nicehour == 0
+				nicehour = 12 
+			end
+			["#{nicehour} #{ampm}", hour]
+		end
+	end
+	
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,21 @@
 class Schedule < ActiveRecord::Base
 	belongs_to :user
 	has_many :bookings
+
+	def hour_options
+		(opening...closing).map do |hour|
+			if hour >= 12
+				nicehour = hour - 12
+				ampm = "pm"
+			else
+				nicehour = hour
+				ampm = "am"
+			end
+			if nicehour == 0
+				nicehour = 12 
+			end
+			["#{nicehour} #{ampm}", hour]
+		end
+	end
+
 end
