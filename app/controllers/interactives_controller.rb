f627ae3d325a51ffa028f3ed01f8d1c95ef698e3
ikhakoo <ikhakoo@gmail.com>
2015-06-06 02:42:35 -0400

    working with password not without password

--- app/controllers/interactives_controller.rb
+++ app/controllers/interactives_controller.rb
@@ -66,7 +66,6 @@ class InteractivesController < ApplicationController
 
    if @room && (@pass = @room.password)
       session[:authenticated_rooms] = @room.id
-      @session = session[:authenticated_rooms]
       redirect_to interactive_path(@room.id)
       flash.now[:notice] = "Login Successful!"
     else
