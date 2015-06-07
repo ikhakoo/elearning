d5a138ce93b10942e622ace703340223ce4206c2
ikhakoo <ikhakoo@gmail.com>
2015-06-06 21:47:05 -0400

    adding some comments to the code

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,26 +7,32 @@ class User < ActiveRecord::Base
   has_many :courses, through: :enrollments
   has_and_belongs_to_many :chapters_completed, :class_name => "Chapter", :join_table => "chapters_users"
 
+  #check if chapter is completed
   def completed?(chapter)
     chapters_completed.include? chapter
   end
 
+  #calculate progress of lesson
   def progress(lesson)
     "%0.2f%" % (chapters_completed.where(lesson: lesson).count / lesson.chapters.count.to_f * 100)
   end
 
+  #calculate if the lesson is completed
   def completed_lesson?(lesson)
     chapters_completed.where(lesson: lesson).count == lesson.chapters.count 
   end
 
+  #check if admin
   def is_admin?
   	self.role != 'instructor' && self.role != 'student'
   end
 
+  #check if instructor
   def is_instructor?
   	self.role != 'admin' && self.role != 'student'
   end
 
+  #check if student
   def is_student?
     self.role != 'instructor' && self.role != 'admin'
   end
@@ -34,6 +40,7 @@ class User < ActiveRecord::Base
 
   before_save :ensure_there_is_a_role
 
+  #set role to student prior to initial registration
   def ensure_there_is_a_role
     if role.blank?
       self.role = "student"
